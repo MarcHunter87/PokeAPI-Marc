d50df7bc514b4bfd8b1239bee41f8bf493906d46
MarcHunter87 <marchunter888@gmail.com>
2025-02-24 19:57:12 +0100

    Revert "a"
    This reverts commit 33b3fbd3a01fc91cc331ae1aff284127593b5e66.

--- pokeapi_pokedex/lib/screens/home_page.dart
+++ pokeapi_pokedex/lib/screens/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokeapi_pokedex/modelos/pokemon.dart';
 import 'package:pokeapi_pokedex/servicios/pokeapi.dart';
-import 'package:pokeapi_pokedex/widgets/pokemon_card.dart';
-import 'package:pokeapi_pokedex/widgets/pokemon_search_bar.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -96,35 +94,75 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: const Color.fromRGBO(33, 33, 33, 1),
         surfaceTintColor: Colors.transparent,
       ),
+
       body: _cargandoPokemons
           ? const Center(child: CircularProgressIndicator(color: Colors.red))
           : RefreshIndicator(
-              onRefresh: _cargarOtraVezLosPokemons,
-              child: ListView.builder(
-                controller: _controladorScroll,
-                itemCount: 1 + pokemonsBuscados.length + (_cargandoMasPokemons ? 1 : 0),
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return PokemonSearchBar(
-                      onChanged: (value) {
-                        setState(() {
-                          _queryDeBusqueda = value;
-                        });
-                      },
-                    );
-                  }
-                  if (index <= pokemonsBuscados.length) {
-                    final pokemon = pokemonsBuscados[index - 1];
-                    return PokemonCard(pokemon: pokemon);
-                  }
-                  return const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: Center(
-                        child: CircularProgressIndicator(color: Colors.red)),
-                  );
-                },
-              ),
-            ),
+        onRefresh: _cargarOtraVezLosPokemons,
+        child: ListView.builder(
+          controller: _controladorScroll,
+          itemCount: 1 + pokemonsBuscados.length + (_cargandoMasPokemons ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 16, left: 16, top: 16, bottom: 8),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Buscar Pokémons',
+                    hintStyle: const TextStyle(color: Color.fromRGBO(189, 189, 189, 1)),
+                    fillColor: const Color.fromRGBO(48, 48, 48, 1),
+                    filled: true,
+                    prefixIcon: const Icon(Icons.search, color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                  onChanged: (value) {
+                    setState(() {
+                      _queryDeBusqueda = value;
+                    });
+                  },
+                ),
+              );
+            }
+            if (index <= pokemonsBuscados.length) {
+              final pokemon = pokemonsBuscados[index - 1];
+              return Card(
+                color: const Color.fromRGBO(48, 48, 48, 1),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 5,
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      pokemon.imageUrl,
+                      width: 60,
+                      height: 60,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  title: Text(
+                    pokemon.name,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white),
+                  ),
+                ),
+              );
+            }
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Center(child: CircularProgressIndicator(color: Colors.red)),
+            );
+          },
+        ),
+      ),
     );
   }
 }
